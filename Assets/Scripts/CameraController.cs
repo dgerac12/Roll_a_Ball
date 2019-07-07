ca96d79abfd04d9bc1e94b2ecd6d4ab2d4bc3652
dgerac12 <dgeraci@hotmail.com>
2019-07-06 21:31:40 -0400

    second commit

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -12,6 +12,11 @@ public class CameraController : MonoBehaviour
         offset = transform.position - player.transform.position;
     }
 
+    void Update () {
+         if (Input.GetKey("escape"))
+     Application.Quit();
+    }
+
     void LateUpdate () {
         transform.position = player.transform.position + offset;
     }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,9 +8,15 @@ public class PlayerController : MonoBehaviour {
     public float speed;
     public Text countText;
     public Text winText;
+    public Text scoreText;
+    public Text livesText;
+
+    public Text overText;
 
     private Rigidbody rb;
     private int count;
+    private int score;
+    private int lives;
 
     void Start ()
     {
@@ -18,12 +24,22 @@ public class PlayerController : MonoBehaviour {
         count = 0;
         SetCountText ();
         winText.text = "";
+        overText.text = "";
+
+        score = 0;
+        SetScoreText ();
+
+        lives = 3;
+        SetLivesText ();
     }
 
     void Update ()
     {
-        if (Input.GetKey("escape"))
-     Application.Quit();
+     if (lives == 0)
+      { 
+          Destroy(this);
+          overText.text = "Game Over";
+      }
     }
 
     void FixedUpdate ()
@@ -42,15 +58,49 @@ public class PlayerController : MonoBehaviour {
       {
           other.gameObject.SetActive (false);
           count = count + 1;
+          score = score + 1;
           SetCountText ();
-      }  
+          SetScoreText ();
+          SetLivesText ();
+      }
+
+      if (score == 12)
+      {
+          transform.position = new Vector3(-60.0f, transform.position.y, 0.0f);
+      }
+
+      else if (other.gameObject.CompareTag("Enemy"))
+      {
+          other.gameObject.SetActive(false);
+          
+          count = count + 1;
+          lives = lives - 1;
+          SetCountText ();
+          SetScoreText ();
+          SetLivesText ();
+      }
+      
     }
 
     void SetCountText ()
     {
         countText.text = "Count: " + count.ToString ();
-        if (count >= 12) {
+        if (score >= 20) {
             winText.text = "You Win!";
+            GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+            foreach(GameObject enemy in enemies)
+            GameObject.Destroy(enemy);
+
         }
+    }
+
+    void SetScoreText ()
+    {
+        scoreText.text = "Score: " + score.ToString ();
+    }
+
+    void SetLivesText ()
+    {
+        livesText.text = "Lives: " + lives.ToString ();
     }
 }
